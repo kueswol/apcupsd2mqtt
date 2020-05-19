0edaa2f75df9140c7ba1cbfe98059327e521af23
Hameedullah Khan <h@hameedullah.com>
2020-05-20 02:17:45 +0300

    Fix command line arguments

--- config.js
+++ config.js
@@ -1,26 +1,26 @@
 module.exports = require('yargs')
     .env('APCUPSD2MQTT')
     .usage('Usage: $0 [options]')
-    .describe('mqtt-url', 'mqtt broker url.')
+    .describe('mqtturl', 'mqtt broker url.')
     .describe('name', 'topic prefix')
-    .describe('ups-name', 'UPS Name. Is overwritten if apcupsd supplies a name')
+    .describe('ups', 'UPS Name. Is overwritten if apcupsd supplies a name')
     .describe('interval', 'Interval in seconds to poll apcaccess')
-    .describe('publish-changes-only', 'Don\'t publish unchanged values')
-    .boolean('publish-changes-only')
+    .describe('changes', 'Don\'t publish unchanged values')
+    .boolean('changes')
     .describe('verbosity', 'possible values: "error", "warn", "info", "debug"')
     .alias({
         h: 'help',
         i: 'interval',
-        m: 'mqtt-url',
+        m: 'mqtturl',
         n: 'name',
-        u: 'ups-name',
+        u: 'ups',
         v: 'verbosity'
     })
     .default({
         interval: 10,
-        mqtt-url: 'mqtt://127.0.0.1',
+        mqtturl: 'mqtt://127.0.0.1',
         name: 'ups',
-        ups-name: 'ups',
+        ups: 'ups',
         verbosity: 'info'
     })
     .version()
